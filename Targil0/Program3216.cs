ded323d9697f002661b17719c317c9adb5119e31
ayelet2022 <adadon@g.jct.ac.il>
2021-10-12 20:30:26 +0300

    Intail code + Welcome function

---
+++ Targil0/Program3216.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3216();
+            Welcome6085();
+            Console.ReadKey();
+        }
+        static partial void Welcome6085();
+        private static void Welcome3216()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
