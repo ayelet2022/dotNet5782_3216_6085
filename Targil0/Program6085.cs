3dc575c80e4cd7e841107fd526dd574a962d5e5b
penina2022 <prabi@g.jct.ac.il>
2021-10-12 20:48:56 +0300

    hi sent you a massage

---
+++ Targil0/Program6085.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome6085()
+        {
+            Console.WriteLine("my name is penina");
+        }
+    }
+}
