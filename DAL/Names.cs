b6d904967f6fccbbdf0585aeaf41721f47f60d0e
ayelet2022 <adadon@g.jct.ac.il>
2021-10-20 12:11:15 +0300

    new classes

--- DAL/Names.cs
+++ DAL/Names.cs
@@ -6,8 +6,9 @@ using DAL;
      {
         public enum WeightCategories { light, inbetween, heavy };
         public enum DroneStatuses { available,inFix, delivery};
-       
-     }
+        public enum Priorities { regular, fast, urgent };
+
+    }
   
 
  }
@@ -15,7 +16,23 @@ namespace DalObject
 {
     class DataSource
     {
-        internal static Drone[] drones = new Drone[10];
+        static Drone[] drones = new Drone[10];
+        static BaseStation[] stations = new BaseStation[5];
+        static Customer[] customers = new Customer[100];
+        static Parcel[] parcels = new Parcel[1000];
+        class Config
+        {
+            static int dronesIndex = 0;
+            static int stationsIndex = 0;
+            static int customersIndex = 0;
+            static int parcelsIndex = 0;
+            int i = 0;
+        }
+        static void Initialize()
+        {
+            drones[dronesIndex].battery = 100;
+            drones[0].battery = 100;
+        }
 
     }
 }
---
+++ DAL/Parcel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+namespace DAL
+{
+    public struct Parcel
+    {
+        public int id { get; set; }
+        public int senderId { get; set; }
+        public int targetId { get; set; }
+        public int droneId { get; set; }
+        public WeightCategories weight { get; set; }
+        public Priorities priority { get; set; }
+        public DateTime creatParcel { get; set; }
+        public DateTime paired { get; set; }
+        public DateTime pickedUp { get; set; }
+        public DateTime arrival { get; set; }
+    }
+}
