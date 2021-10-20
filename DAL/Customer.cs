a6175f00e80fdda6e464fe63a9a8ce7b7672b71a
penina2022 <prabi@g.jct.ac.il>
2021-10-20 15:15:32 +0300

    hi

--- DAL/Customer.cs
+++ DAL/Customer.cs
@@ -3,27 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DAL.IDAL.DO
+
 namespace DAL
 {
     public struct Customer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-
-        public override string ToString()
-        {
-            String result = " ";
-            result += $"ID is {Id}, \n";
-            result += $"Name is {Name}, \n";
-            result += $"Telephone is {Phone.Substring(0, 3) + '-' + Phone.Substring(3)}, \n";
-            result += $"Latitude is {Latitude}, \n";
-            result += $"longitude is {Longitude}, \n";
-            return result;
-        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public double longitud { get; set; }
+        public double latitude { get; set; }
 
     }
 }
--- DAL/Names.cs
+++ DAL/Names.cs
@@ -1,174 +1,83 @@
 ﻿using System;
-using DAL.IDAL.DO;
+using DAL;
+using IDAL.DO;
+Random rand = new Random(DateTime.Now.Millisecond);
 
-Random Rand = new Random(DateTime.Now.Millisecond);
-
-namespace DAL
+namespace DalObject
 {
-    namespace DalObject
+    class DataSource
     {
-        public class DalObject
+        static Drone[] drones = new Drone[10];
+        static BaseStation[] stations = new BaseStation[5];
+        static Customer[] customers = new Customer[100];
+        static Parcel[] parcels = new Parcel[1000];
+        internal class Config
         {
-            DalObject()
+            internal static int dronesIndex = 0;
+            internal static int stationsIndex = 0;
+            internal static int customersIndex = 0;
+            internal static int parcelsIndex = 0;
+            internal int i = 0;
+            public int DronesIndex
             {
-                DataSource.Initialize();
+                get => dronesIndex;
+                set { dronesIndex = value; }
             }
-        }
-        class DataSource
-        {
-            static Drone[] Drones = new Drone[10];
-            static BaseStation[] Stations = new BaseStation[5];
-            static Customer[] Customers = new Customer[100];
-            static Parcel[] Parcels = new Parcel[1000];
-            internal class Config
+            static void Initialize()
             {
-                static int DronesIndex = 0;
-                static int StationsIndex = 0;
-                static int CustomersIndex = 0;
-                static int ParcelsIndex = 0;
-                int I = 0;
-            }
-            public static void Initialize()
-            {
-                for(int i=0;i<2;i++)
-                {
-                    Stations[i].Id = Rand.Next(0,1000000);
+                drones[0].id = rand(0,10);
+                drones[0].battery = 100;
+                drones[0].id = 1111;
+                drones[0].maxWeight = (WeightCategories)0;
+                drones[0].model = "aaa";
+                drones[0].status = (DroneStatuses)0;
 
-                }
+                drones[dronesIndex].battery = 90;
+                drones[1].id = 2222;
+                drones[1].maxWeight = (WeightCategories)1;
+                drones[1].model = "bbb";
+                drones[1].status = (DroneStatuses)1;
 
+                drones[dronesIndex].battery = 80;
+                drones[2].id = 3333;
+                drones[2].maxWeight = (WeightCategories)2;
+                drones[2].model = "ccc";
+                drones[2].status = (DroneStatuses)2;
 
+                drones[dronesIndex].battery = 70;
+                drones[3].id = 4444;
+                drones[3].maxWeight = (WeightCategories)0;
+                drones[1].model = "ddd";
+                drones[1].status = (DroneStatuses)1;
 
+                drones[dronesIndex].battery = 60;
+                drones[1].id = 4444;
+                drones[1].maxWeight = (WeightCategories)1;
+                drones[1].model = "yyy";
+                drones[1].status = (DroneStatuses)1;
 
+                stations[0].emptyCharges = 4;
+                stations[0].id = 264;
+                stations[0].latitude = 26.8;
+                stations[0].longitud = 50;
+                stations[0].name = "jerusalem";
 
-
-
-
-
-                drones[DataSource.Config.dronesIndex].battery = rand.Next(0, 100);
-                drones[DataSource.Config.dronesIndex].id = 1111;
-                drones[DataSource.Config.dronesIndex].maxWeight = (WeightCategories)0;
-                drones[DataSource.Config.dronesIndex].model = "aaa";
-                drones[DataSource.Config.dronesIndex].status = (DroneStatuses)0;
-                DataSource.Config.dronesIndex++;
-                drones[DataSource.Config.dronesIndex].battery = 90;
-                drones[DataSource.Config.dronesIndex].id = 2222;
-                drones[DataSource.Config.dronesIndex].maxWeight = (WeightCategories)1;
-                drones[DataSource.Config.dronesIndex].model = "bbb";
-                drones[DataSource.Config.dronesIndex].status = (DroneStatuses)1;
-                DataSource.Config.dronesIndex++;
-                drones[DataSource.Config.dronesIndex].battery = 80;
-                drones[DataSource.Config.dronesIndex].id = 3333;
-                drones[DataSource.Config.dronesIndex].maxWeight = (WeightCategories)2;
-                drones[DataSource.Config.dronesIndex].model = "ccc";
-                drones[DataSource.Config.dronesIndex].status = (DroneStatuses)2;
-                DataSource.Config.dronesIndex++;
-                drones[DataSource.Config.dronesIndex].battery = 70;
-                drones[DataSource.Config.dronesIndex].id = 4444;
-                drones[DataSource.Config.dronesIndex].maxWeight = (WeightCategories)0;
-                drones[DataSource.Config.dronesIndex].model = "ddd";
-                drones[DataSource.Config.dronesIndex].status = (DroneStatuses)1;
-                DataSource.Config.dronesIndex++;
-                drones[DataSource.Config.dronesIndex].battery = 60;
-                drones[DataSource.Config.dronesIndex].id = 4444;
-                drones[DataSource.Config.dronesIndex].maxWeight = (WeightCategories)1;
-                drones[DataSource.Config.dronesIndex].model = "yyy";
-                drones[DataSource.Config.dronesIndex].status = (DroneStatuses)1;
-                DataSource.Config.dronesIndex++;
-
-                stations[DataSource.Config.stationsIndex].emptyCharges = 4;
-                stations[DataSource.Config.stationsIndex].id = 264;
-                stations[DataSource.Config.stationsIndex].latitude = 26.8;
-                stations[DataSource.Config.stationsIndex].longitud = 50;
-                stations[DataSource.Config.stationsIndex].name = "jerusalem";
-                DataSource.Config.stationsIndex++;
-                stations[DataSource.Config.stationsIndex].emptyCharges = 4;
-                stations[DataSource.Config.stationsIndex].id = 264;
-                stations[DataSource.Config.stationsIndex].latitude = 26.8;
-                stations[DataSource.Config.stationsIndex].longitud = 50;
-                stations[DataSource.Config.stationsIndex].name = "jerusalem";
-                DataSource.Config.stationsIndex++;
-
-                customers[DataSource.Config.customersIndex].id = 111111111;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Ayelet";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 222222222;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Penina";
-                customers[DataSource.Config.customersIndex].phone = "0548538699";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 333333333;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Yoni";
-                customers[DataSource.Config.customersIndex].phone = "0544828416";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 333333333;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Chani";
-                customers[DataSource.Config.customersIndex].phone = "0548459415";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 444444444;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Nomi";
-                customers[DataSource.Config.customersIndex].phone = "0548459416";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 555555555;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Yosi";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].Id = 666666666;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Avi";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].Id = 777777777;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Talya";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 888888888;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Chaya";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-                customers[DataSource.Config.customersIndex].id = 999999999;
-                customers[DataSource.Config.customersIndex].latitude = 111111111;
-                customers[DataSource.Config.customersIndex].longitud = 111111111;
-                customers[DataSource.Config.customersIndex].name = "Michal";
-                customers[DataSource.Config.customersIndex].phone = "0548735177";
-                DataSource.Config.customersIndex++;
-
-                parcels[DataSource.Config.parcelsIndex].id = 121212121;
-                parcels[DataSource.Config.parcelsIndex].paired = DateTime.Now;
-                parcels[DataSource.Config.parcelsIndex].pickedUp = DateTime.Now;
-                parcels[DataSource.Config.parcelsIndex].priority = (Priorities)1;
-                parcels[DataSource.Config.parcelsIndex].senderId = 111111111;
-                parcels[DataSource.Config.parcelsIndex].targetId = 111111111;
+                stations[0].emptyCharges = 4;
+                stations[0].id = 264;
                 Random r = new Random();
-                r.nex
-                parcels[DataSource.Config.parcelsIndex].weight = r;
+                stations[0].latitude = r;
+                stations[0].longitud = 50;
+                stations[0].name = "jerusalem";
 
+                drones[1].model = "eee";
+                drones[1].status = (DroneStatuses)2;
 
-
-
-
-
-
-
+                customers[0].id =
             }
-
+       
         }
+        
+
     }
 }
 
