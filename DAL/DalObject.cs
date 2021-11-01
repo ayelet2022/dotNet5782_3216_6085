f2ceaefb4dcd764ebfec1d0204e9a97e31fb1c29
ayelet2022 <adadon@g.jct.ac.il>
2021-11-01 18:30:44 +0200

    new

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -39,7 +39,7 @@ namespace DAL
             /// <summary>
             /// adds a new customer to the arrey
             /// </summary>
-            /// <param name="newCustomer"></param>
+            /// <param name="newCustomer"></param>the new customer that the user entered in main and needs to be added to the arrey
             public static void AddCustomer(Customer newCustomer)
             {
                 DataSource.Customers[DataSource.Config.CustomersIndex].Id = newCustomer.Id;
@@ -52,7 +52,7 @@ namespace DAL
             /// <summary>
             /// adds a new parcel to the arrey
             /// </summary>
-            /// <param name="newParcel"></param>
+            /// <param name="newParcel"></param>the new parce that the user entered in main and needs to be added to the arrey
             public static void AddParcel(Parcel newParcel)
             {
                 DataSource.Parcels[DataSource.Config.ParcelsIndex].CreatParcel = DateTime.Now;
@@ -67,109 +67,116 @@ namespace DAL
                 DataSource.Parcels[DataSource.Config.ParcelsIndex].Weight = newParcel.Weight;
                 DataSource.Config.ParcelsIndex++;
             }
+
             /// <summary>
-            ///  returnes the base sation that the user wants to print according to his id
+            ///  search for the base station in the arrey thet has the same id that the user enterd and returnes it
             /// </summary>
-            /// <param name="idBaseStation"></param>
-            /// <returns></returns>
+            /// <param name="idBaseStation"></param>the id(that was enterd by the user in main) of the basestation that the user wants to print
+            /// <returns></returns>resturn the base station that needs to be printed
             public static BaseStation PrintBaseStation(int idBaseStation)
             {
                 int baseStationIndex = 0;
-                while (DataSource.Stations[baseStationIndex].Id != idBaseStation)
+                while (DataSource.Stations[baseStationIndex].Id != idBaseStation)//search for the base station that has the same id has the id that the user enterd
                     baseStationIndex++;
-                return (DataSource.Stations[baseStationIndex]);
+                return DataSource.Stations[baseStationIndex];
 
             }
 
             /// <summary>
-            ///  returnes the drone that the user wantes to print according to his id
+            ///  search for the drone in the arrey thet has the same id as the user enterd and returnes it
             /// </summary>
-            /// <param name="idDrone"></param>
-            /// <returns></returns>
+            /// <param name="idDrone"></param>the id(that was enterd by the user in main) of the dron that the user wants to print
+            /// <returns></returns>resturn the drone that needs to be printed
             public static Drone PrintDrone(int idDrone)
             {
                 int droneIndex = 0;
-                while (DataSource.Drones[droneIndex].Id != idDrone)
+                while (DataSource.Drones[droneIndex].Id != idDrone)//search for the drone that has the same id has the id that the user enterd
                     droneIndex++;
-                return (DataSource.Drones[droneIndex]);
-
+                return DataSource.Drones[droneIndex];
             }
 
+            /// <summary>
+            /// search for the customer in the arrey thet has the same id as the user enterd and retirnes it
+            /// </summary>
+            /// <param name="idCustomer"></param>the id(that was enterd by the user in main) of the customer that the user wants to print
+            /// <returns></returns>resturn the customer that needs to be printed
             public static Customer PrintCustomer(int idCustomer)
             {
                 int customerIndex = 0;
-                while (DataSource.Customers[customerIndex].Id != idCustomer)
+                while (DataSource.Customers[customerIndex].Id != idCustomer)//search for the customer that has the same id has the id that the user enterd
                     customerIndex++;
                 return (DataSource.Customers[customerIndex]);
 
             }
+
             /// <summary>
-            /// returnes the parcel that the user wanted to print according to his id
+            /// search for the parcel in the arrey thet has the same id as the user enterd and retirnes it
             /// </summary>
-            /// <param name="idParcel"></param> the id of the parcel the the user wants to print
+            /// <param name="idParcel"></param> the id(that was enterd by the user in main) of the parcel that the user wants to print
             /// <returns></returns>the parcel that needs to be printed
             public static Parcel PrintParcel(int idParcel)
             {
                 int parcelIndex = 0;
-                while (DataSource.Parcels[parcelIndex].Id != idParcel)
+                while (DataSource.Parcels[parcelIndex].Id != idParcel)//search for the parcel that has the same id has the id that the user enterd
                     parcelIndex++;
                return (DataSource.Parcels[parcelIndex]);
 
             }
+
             /// <summary>
-            /// looks for a free drone that can the delever the parcel to the customer
-            /// </summary>
-            /// <param name="newParcel"></param> the parcel thats the dron need to deliver
-            /// <returns></returns>the id of the drone that can deliver the parcel
+            /// search for the drone and the parcel that has the same id as what the user enterd 
+            /// and changes the filed of the drone id inn the parcel detailes</summary>
+            /// <param name="droneId"></param>the drones id that the user enterd
+            /// <param name="parcelId"></param>the parcel id that the user enterd
             public static void DronToAParcel(int droneId, int parcelId)
             {
                 int pareclIndex = 0;
-                while (DataSource.Parcels[pareclIndex].Id != parcelId)
+                while (DataSource.Parcels[pareclIndex].Id != parcelId)//search for the parcel that has the same id has the id that the user enterd
                     pareclIndex++;
                 int droneInsex = 0;
-                while (DataSource.Drones[droneInsex].Id != droneId)
+                while (DataSource.Drones[droneInsex].Id != droneId)//search for the drone that has the same id has the id that the user enterd
                     droneInsex++;
                 DataSource.Parcels[pareclIndex].DroneId = DataSource.Drones[droneInsex].Id;
                 DataSource.Parcels[pareclIndex].Scheduled = DateTime.Now;
             }
 
             /// <summary>
-            /// looks for an empty charger in the base station so the drone will be able to charge there 
-            /// and updates all that needs to be update becaus of that
+            /// update the drone that has the same id as the user enterd to charge in the base station that has the id that the user enterd
             /// </summary>
-            /// <param name="dronToCharge"></param> the drone that need charging
+            /// <param name="dronesId"></param>the drones id that the user enterd
+            /// <param name="idOfBaseStation"></param>the base station id that the user enterd
             public static void DronToCharger(int dronesId, int idOfBaseStation)
             {
                 int droneIndex = 0;
-                while (DataSource.Drones[droneIndex].Id != dronesId)
+                while (DataSource.Drones[droneIndex].Id != dronesId)//search for the drone that has the same id has the id that the user enterd
                     droneIndex++;
                 int stationsIndex = 0;
-                while (DataSource.Stations[stationsIndex].Id != idOfBaseStation)
+                while (DataSource.Stations[stationsIndex].Id != idOfBaseStation)//search for the base station that has the same id has the id that the user enterd
                     stationsIndex++;
                 DataSource.DroneCharges[DataSource.Config.DroneChargesIndex].DroneId = DataSource.Drones[droneIndex].Id;
                 DataSource.DroneCharges[DataSource.Config.DroneChargesIndex].StationId = DataSource.Stations[stationsIndex].Id;
                 DataSource.Config.DroneChargesIndex++;
                 DataSource.Stations[stationsIndex].EmptyCharges--;
-                DataSource.Drones[droneIndex].Status = (DroneStatuses)1;
+                DataSource.Drones[droneIndex].Status = (DroneStatuses)1;//available
             }
 
             /// <summary>
-            /// free the drone from the charger that he was cherging from in the base station
+            /// free the drone-that has the same id that the user enterd from the charger that he was cherging from in the base station
             /// </summary>
-            /// <param name="dronToFree"></param>the drone that needs to get free from the charger he his charging from
+            /// <param name="dronesId"></param>the drones id that the user enterd
             public static void FreeDroneFromBaseStation(int dronesId)
             {
                 int droneLocation = 0;
-                while (DataSource.Drones[droneLocation].Id != dronesId)
+                while (DataSource.Drones[droneLocation].Id != dronesId)//search for the drone that has the same id has the id that the user enterd
                     droneLocation++;
                 DataSource.Drones[droneLocation].Battery = 100;
-                DataSource.Drones[droneLocation].Status = (DroneStatuses)0;
+                DataSource.Drones[droneLocation].Status = (DroneStatuses)0;//availabel
                 int droneChargesIndex = 0;
-                while (DataSource.DroneCharges[droneChargesIndex].DroneId != dronesId)
+                while (DataSource.DroneCharges[droneChargesIndex].DroneId != dronesId)//search for the drone charge that is chargine the drone that has the same id as the user enterd
                     droneChargesIndex++;
-                DataSource.DroneCharges[droneChargesIndex].DroneId = 0;
+                DataSource.DroneCharges[droneChargesIndex].DroneId = 0;//frees the charger
                 int stationLocation = 0;
-                while (DataSource.Stations[stationLocation].Id != DataSource.DroneCharges[droneChargesIndex].StationId)
+                while (DataSource.Stations[stationLocation].Id != DataSource.DroneCharges[droneChargesIndex].StationId)//search for the base station that has the same id as the one in the chrger
                     stationLocation++;
                 DataSource.Stations[stationLocation].EmptyCharges++;
                 DataSource.DroneCharges[droneChargesIndex].StationId = 0;
@@ -178,11 +185,11 @@ namespace DAL
             /// <summary>
             /// updates the time that the parcel was picked up by the dron
             /// </summary>
-            /// <param name="ParclToPickup"></param> the parcel that needs delevering
+            /// <param name="newId"></param>the id pf the parcel that was enterd by the user
             public static void PickUpParcel(int newId)
             {
                 int parcelsIndex = 0;
-                while (DataSource.Parcels[parcelsIndex].Id != newId)
+                while (DataSource.Parcels[parcelsIndex].Id != newId)//search for the parcel that has the same id has the id that the user enterd
                     parcelsIndex++;
                 DataSource.Parcels[parcelsIndex].PickedUp = DateTime.Now;
             }
@@ -190,23 +197,23 @@ namespace DAL
             /// <summary>
             /// updates the time that the parcel was deleverd to the customer
             /// </summary>
-            /// <param name="ParcelDeliverd"></param>
+            /// <param name="newId"></param>the id pf the parcel that was enterd by the user
             public static void ParcelToCustomer(int newId)
             {
                 int parcelsIndex = 0;
-                while (DataSource.Parcels[parcelsIndex].Id != newId)
+                while (DataSource.Parcels[parcelsIndex].Id != newId)//search for the parcel that has the same id has the id that the user enterd
                     parcelsIndex++;
                 DataSource.Parcels[parcelsIndex].Delivered = DateTime.Now;
             }
 
-
             /// <summary>
-            /// prints the whole list of the object according to what the user enterd
+            /// copyes the values of al the base stations in order to print them
             /// </summary>
+            /// <returns></returns>the new arrey that has the the base stations
             public static BaseStation[] PrintBaseStations()
             {
                 BaseStation[] ActiveStations = new BaseStation[DataSource.Config.StationsIndex]; 
-                for (int i = 0; i < DataSource.Config.StationsIndex; i++)
+                for (int i = 0; i < DataSource.Config.StationsIndex; i++)//to copy all of the base station to the new arrey
                 {
                     ActiveStations[i].Id = DataSource.Stations[i].Id;
                     ActiveStations[i].Name = DataSource.Stations[i].Name;
@@ -216,10 +223,15 @@ namespace DAL
                 }
                 return ActiveStations;
             }
+
+            /// <summary>
+            /// copyes the values of all the drones in order to print them
+            /// </summary>
+            /// <returns></returns>the new arrey that has the the drones
             public static Drone[] PrintDrones()
             {
                 Drone[] ActiveDrones = new Drone[DataSource.Config.DronesIndex];
-                for (int i = 0; i < DataSource.Config.DronesIndex; i++)
+                for (int i = 0; i < DataSource.Config.DronesIndex; i++)//to copy all of the drones to the new arrey
                 {
                     ActiveDrones[i].Id = DataSource.Drones[i].Id;
                     ActiveDrones[i].MaxWeight = DataSource.Drones[i].MaxWeight;
@@ -229,10 +241,14 @@ namespace DAL
                 }
                 return ActiveDrones;
             }
+            /// <summary>
+            /// copyes the values of all the customer in order to print them
+            /// </summary>
+            /// <returns></returns>the new arrey that has the the customers
             public static Customer[] PrintCustomers()
             {
                 Customer[] ActiveCustomers = new Customer[DataSource.Config.CustomersIndex];
-                for (int i = 0; i < DataSource.Config.CustomersIndex; i++)
+                for (int i = 0; i < DataSource.Config.CustomersIndex; i++)//to copy all of the customers to the new arrey
                 {
                     ActiveCustomers[i].Id = DataSource.Customers[i].Id;
                     ActiveCustomers[i].Latitude = DataSource.Customers[i].Latitude;
@@ -242,10 +258,14 @@ namespace DAL
                 }
                 return ActiveCustomers;
             }
+            /// <summary>
+            /// copyes the values of all the parcel in order to print them
+            /// </summary>
+            /// <returns></returns>the new arrey that has the the parceles
             public static Parcel[] PrintPercels()
             {
                 Parcel[] ActiveParcels = new Parcel[DataSource.Config.ParcelsIndex];
-                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)
+                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)//to copy all of the parceles to the new arrey
                 {
                     ActiveParcels[i].Id = DataSource.Parcels[i].Id;
                     ActiveParcels[i].PickedUp = DataSource.Parcels[i].PickedUp;
@@ -260,15 +280,19 @@ namespace DAL
                 }
                 return ActiveParcels;
             }
+            /// <summary>
+            /// search for all the drones that are available and copy them to a new arrey  
+            /// </summary>
+            /// <returns></returns>the new arrey that has all the drones that are availeble
             public static Parcel[] ParcelThatWerenNotPaired()
             {
                 int count = 0;
-                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)
+                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)//counts how many available drones ther are
                     if (DataSource.Parcels[i].DroneId == 0)
                         count++;
-                Parcel[] Parcels = new Parcel[count];
-                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)
-                    if (DataSource.Parcels[i].DroneId == 0)
+                Parcel[] Parcels = new Parcel[count];//makes a new arrey in the size of the available drones
+                for (int i = 0; i < DataSource.Config.ParcelsIndex; i++)//to copy all the available drones
+                    if (DataSource.Parcels[i].DroneId == 0)//if the dron is available copy his dalaies
                     {
                         Parcels[i].Id = DataSource.Parcels[i].Id;
                         Parcels[i].PickedUp = DataSource.Parcels[i].PickedUp;
@@ -283,15 +307,19 @@ namespace DAL
                     }
                 return Parcels;
             }
+            /// <summary>
+            /// search for tha base station that has available charges and copys them to a new arrey
+            /// </summary>
+            /// <returns></returns>//the new arrey that has all the base station that has free charges
             public static BaseStation[] BaseStationWithAvailableCharges()
             {
                 int count = 0;
-                for (int i = 0; i < DataSource.Config.StationsIndex; i++)
-                    if (DataSource.Stations[i].EmptyCharges > 0)
+                for (int i = 0; i < DataSource.Config.StationsIndex; i++)//checks all the base station for available charges
+                    if (DataSource.Stations[i].EmptyCharges > 0)//meens ther are enmpty charges
                         count++;
-                BaseStation[] Stations = new BaseStation[count];
-                for (int i = 0; i < DataSource.Config.StationsIndex; i++)
-                    if (DataSource.Stations[i].EmptyCharges > 0)
+                BaseStation[] Stations = new BaseStation[count];//a new arrey in the size of the base station that have available charges
+                for (int i = 0; i < DataSource.Config.StationsIndex; i++)//checks all the base station for available charges
+                    if (DataSource.Stations[i].EmptyCharges > 0)//meens ther are enmpty charges so copy the base station
                     {
                        Stations[i].Id = DataSource.Stations[i].Id;
                        Stations[i].Name = DataSource.Stations[i].Name;
@@ -304,7 +332,7 @@ namespace DAL
         }
         /// <summary>
         /// includs all the data of the object-the arr of each object,
-        /// the index of each arr 
+        /// and the index of each arr 
         /// </summary>
     class DataSource
         {
