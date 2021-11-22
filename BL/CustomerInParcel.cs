a6681da03ef931a7b4aa47c81d223c23c0ecc017
ayelet2022 <adadon@g.jct.ac.il>
2021-11-22 13:16:12 +0200

    hi

--- BL/CustomerInParcel.cs
+++ BL/CustomerInParcel.cs
@@ -9,12 +9,12 @@ namespace IBL.BO
     public class CustomerInParcel
     {
         public int Id { get; set; }
-        public string CustomerName { get; set; }
+        public string Name { get; set; }
         public override string ToString()
         {
             String result = "";
             result += $"ID is {Id}, \n";
-            result += $"Name is {CustomerName}, \n";
+            result += $"Name is {Name}, \n";
             return result;
         }
     }
