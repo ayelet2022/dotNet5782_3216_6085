b5ae299150e11a79beb09b2f7995072920118a9e
penina2022 <prabi@g.jct.ac.il>
2021-11-30 11:08:31 +0200

    hi

--- BL/Parcel.cs
+++ BL/Parcel.cs
@@ -8,27 +8,12 @@ namespace IBL.BO
 {
     public class Parcel
     {
-        /// <summary>
-        /// the parcel id
-        /// </summary>
         public int Id { get; set; }
         public CustomerInParcel Sender { get; set; }
         public CustomerInParcel Recepter { get; set; }
-        /// <summary>
-        /// the parcel whight
-        /// </summary>
         public WeightCategories Weight { get; set; }
-        /// <summary>
-        /// the parcel priority
-        /// </summary>
         public Priorities Priority { get; set; }
-        /// <summary>
-        /// the time that the parcel was created
-        /// </summary>
         public DateTime? CreatParcel { get; set; }
-        /// <summary>
-        /// the time that a drone was peered to the parcel
-        /// </summary>
         public DateTime? Scheduled { get; set; }
         public DateTime? PickedUp { get; set; }
         public DateTime? Delivered { get; set; }
