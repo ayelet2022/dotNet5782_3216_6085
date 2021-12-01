7ae04df6b1fe9c77442645ec5461e3f9518e8239
penina2022 <prabi@g.jct.ac.il>
2021-12-01 23:44:11 +0200

    hi

--- PL/WindowDrone.xaml.cs
+++ PL/WindowDrone.xaml.cs
@@ -20,7 +20,7 @@ namespace PL
     /// </summary>
     public partial class WindowDrone : Window
     {
-        Drone drone = new();
+        Drone mainDrone = new();
         IBL.IBL ibl;
         public WindowDrone(IBL.IBL bl)
         {
@@ -28,13 +28,20 @@ namespace PL
             ibl = bl;
             add.Visibility = Visibility.Visible;
             weightA.ItemsSource = Enum.GetValues(typeof(WeightCategories));
-            drone.Id = (int)IdBoxA.DataContext;
-            drone.Model = (string)ModelBoxA.DataContext;
+            mainDrone.Id = (int)IdBoxA.DataContext;
+            mainDrone.Model = (string)ModelBoxA.DataContext;
         }
         public WindowDrone(Drone drone)
         {
+            Actions.Visibility = Visibility.Visible;
             IdBoxAc.DataContext = drone.Id;
             BatteryBoxAc.DataContext = drone.Battery;
+            status2.SelectedItem = drone.Status;
+            LatitudeBoxAc.DataContext = drone.DroneLocation.Latitude;
+            LongtitudeBoxAc.DataContext = drone.DroneLocation.Longitude;
+            weight2.SelectedItem = drone.MaxWeight;
+            ParcelBoxAc.DataContext = drone.ParcelInTransfer.Id;
+            mainDrone = drone;
         }
 
         private void DronesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -44,17 +51,19 @@ namespace PL
 
         private void weight_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            drone.MaxWeight = (WeightCategories)statusA.SelectedItem;
+            mainDrone.MaxWeight = (WeightCategories)statusA.SelectedItem;
+            add.Visibility = Visibility.Hidden;
         }
 
         private void addButten_Click(object sender, RoutedEventArgs e)
         {
-            ibl.AddDrone(drone, (int)IdStation.DataContext);
+            ibl.AddDrone(mainDrone, (int)IdStation.DataContext);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            ibl.UpdateDrone(mainDrone.Id, (string)ModelAc.DataContext);
+            Actions.Visibility = Visibility.Hidden;
         }
     }
 }
