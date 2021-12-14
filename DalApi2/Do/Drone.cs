71595a0ce9ed18a7817af60cc388ecd144611489
ayelet dadon <adadon@g.jct.ac.il>
2021-12-14 11:56:19 +0200

    hi

---
+++ DalApi2/Do/Drone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DO
+{
+    /// <summary>
+    /// the drones are the way we send the parcel from one place to enother
+    /// </summary>
+    public struct Drone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightCategories MaxWeight { get; set; }
+        public override string ToString()
+        {
+            String result = "";
+            result += $"ID is {Id}, \n";
+            result += $"Model is {Model}, \n";
+            result += $"MaxWeight is {MaxWeight}, \n";
+            return result;
+        }
+    }
+}
+
---
+++ DalApi2/Do/MyException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DO
+{
+    [Serializable]
+    public class ExistsException : Exception
+    {
+        public ExistsException() : base() { }
+        public ExistsException(string message) : base(message) { }
+        public ExistsException(string message, Exception inner) : base(message, inner) { }
+    }
+    public class DoesNotExistException : Exception
+    {
+        public DoesNotExistException() : base() { }
+        public DoesNotExistException(string message) : base(message) { }
+        public DoesNotExistException(string message, Exception inner) : base(message, inner) { }
+    }
+    public class NotFoundInputException : Exception
+    {
+        public NotFoundInputException() : base() { }
+        public NotFoundInputException(string message) : base(message) { }
+        public NotFoundInputException(string message, Exception inner) : base(message, inner) { }
+    }
+}
+
